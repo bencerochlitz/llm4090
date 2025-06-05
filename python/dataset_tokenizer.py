49f992acfd22f8de555c5088a88b8fe4bc73bec6
bencerochlitz <rbence94b@gmail.com>
2025-06-05 20:32:00 +0100

    saved tokenized data to h5 in uint16

--- python/dataset_tokenizer.py
+++ python/dataset_tokenizer.py
@@ -5,13 +5,10 @@ import numpy as np
 import sys
 import os
 import h5py
+import time
 
 
-def load_and_convert_to_h5(path):
-
-    # dataset = load_dataset("Salesforce/wikitext", "wikitext-103-v1")
-    dataset = load_dataset("Salesforce/wikitext", "wikitext-103-raw-v1")
-
+def load_and_convert_to_h5(path, dataset):
     data_train = dataset['train']['text']
     data_val = dataset['validation']['text']
     data_test = dataset['test']['text']
@@ -21,6 +18,9 @@ def load_and_convert_to_h5(path):
     print("test: ", np.shape(data_test))
 
     print("Example data:\n", data_train[0:5])
+
+    if os.path.exists(path):
+        os.remove(path)
 
     with h5py.File(path, 'a') as h5:
         ds = h5.create_dataset('train', shape=len(data_train),
@@ -42,6 +42,10 @@ def load_h5(path):
         d_val = h5['val'][:]
         d_test = h5['test'][:]
 
+    d_train = [s.decode('utf-8') for s in d_train]
+    d_val = [s.decode('utf-8') for s in d_val]
+    d_test = [s.decode('utf-8') for s in d_test]
+
     return d_train, d_val, d_test
 
 
@@ -50,28 +54,80 @@ def encode_dataset(data):
     # Test encoding
     enc = tiktoken.get_encoding("gpt2")  # Load the GPT-2 tokenizer
 
-    # arr = np.array([])
+    arr = []
+    num_threads = 4  # seems to be the sweet spot
+    num_s = len(data)
+    # num_batches = num_s // num_threads
+    print("num_s: ", num_s)
+    print("1e6 takes ~30s")
+    # print("num_batches: ", num_batches)
 
-    for s in data[0:5]:
+    # for i in range(num_batches):
+    # print("encoding batch: ", i)
+    # idx_b = i * num_threads
+    # idx_e = (i + 1) * num_threads
 
-        tokens = enc.encode(s.decode('utf-8'))  # Tokenize the string
-        tokens.append(enc.eot_token)  # Ａdd the end of text token
+    tokens = enc.encode_batch(data, num_threads=num_threads)
 
-        print("tokens[0] type: ", type(tokens[0]))
-        print("tokens[0] size: ", sys.getsizeof(tokens[0]))
+    for t in tokens:
+        t.append(enc.eot_token)  # Ａdd the end of text token
 
-        print(tokens)
+    # tokens = np.asarray(tokens, dtype=np.uint16)
+    # arr.extend(tokens)
 
-        np.append(arr, tokens)
+    # if i < 3:
+    #     # print("tokens[0] type: ", type(tokens[0]))
+    #     # print("tokens[0] size: ", sys.getsizeof(tokens[0]))
+    #     print(s)
+    #     print(tokens)
+    #     print(enc.decode(tokens))
 
-    print("tokenized data: ", arr)
+    # print("tokenized data: ", arr)
+
+    return tokens
+
+
+def save_tokenized_data(path, data_train, data_val, data_test):
+    if os.path.exists(path):
+        os.remove(path)
+
+    dt = h5py.vlen_dtype(np.dtype('uint16'))
+
+    with h5py.File(path, 'a') as h5:
+        ds = h5.create_dataset('train', shape=len(data_train),
+                               dtype=dt)
+        ds[:] = data_train
+
+        ds = h5.create_dataset('val', shape=len(data_val),
+                               dtype=dt)
+        ds[:] = data_val
+
+        ds = h5.create_dataset('test', shape=len(data_test),
+                               dtype=dt)
+        ds[:] = data_test
 
 
 if __name__ == "__main__":
 
     path = './data/wikitext.h5'
-    # load_and_convert_to_h5(path)
+    # # dataset = load_dataset("Salesforce/wikitext", "wikitext-103-v1")
+    # dataset = load_dataset("Salesforce/wikitext", "wikitext-103-raw-v1")
+    # load_and_convert_to_h5(path, dataset)
 
     d_train, d_val, d_test = load_h5(path)
 
-    encode_dataset(d_train)
+    t_s = time.perf_counter()
+
+    print("starting encoding d_train...")
+    d_train = encode_dataset(d_train)
+
+    print("starting encoding d_val...")
+    d_val = encode_dataset(d_val)
+
+    print("starting encoding d_test...")
+    d_test = encode_dataset(d_test)
+
+    print("done encoding, took ", time.perf_counter() - t_s)
+
+    path = './data/wikitext_tok.h5'
+    save_tokenized_data(path, d_train, d_val, d_test)
